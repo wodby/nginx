76f81a8504ec2163bc09693a30198a4d4ac88ec7
Chingis Sandanov <chingis@wodby.com>
2019-01-18 14:47:23 +0700

    Update tests

--- tests/php/run.sh
+++ tests/php/run.sh
@@ -15,17 +15,4 @@ echo -n "Checking php endpoint... "
 docker-compose exec nginx curl "localhost" | grep -q "Hello World!"
 echo "OK"
 
-echo -n "Checking Geo IP detection... "
-brooklyn_ip="161.185.160.93"
-
-docker-compose exec php sh -c 'echo "<?php var_dump(\$_SERVER[\"CITY_NAME\"]);" > /var/www/html/index.php'
-docker-compose exec nginx curl --header "X-Forwarded-For: ${brooklyn_ip}" localhost | grep -q "Brooklyn"
-
-docker-compose exec php sh -c 'echo "<?php var_dump(\$_SERVER[\"COUNTRY_CODE\"]);" > /var/www/html/index.php'
-docker-compose exec nginx curl --header "X-Forwarded-For: ${brooklyn_ip}" localhost | grep -q "US"
-
-docker-compose exec php sh -c 'echo "<?php var_dump(\$_SERVER[\"COUNTRY_NAME\"]);" > /var/www/html/index.php'
-docker-compose exec nginx curl --header "X-Forwarded-For: ${brooklyn_ip}" localhost | grep -q "United States"
-echo "OK"
-
 docker-compose down

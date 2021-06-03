c1b88bb6f94c1a5b55710c07f4910497a02add8e
Chingis Sandanov <chingis@wodby.com>
2021-06-04 00:52:51 +0700

    Additionally push images to wodby registry

--- .github/actions/release.sh
+++ .github/actions/release.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]]; then
-    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}" "${DOCKER_REGISTRY}"
 
     if [[ "${GITHUB_REF}" == refs/tags/* ]]; then
       export STABILITY_TAG="${GITHUB_REF##*/}"
@@ -12,6 +12,6 @@ if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]];
     IFS=',' read -ra tags <<< "${TAGS}"
 
     for tag in "${tags[@]}"; do
-        make buildx-push TAG="${tag}";
+        make buildx-push TAG="${tag}" REGISTRY="${DOCKER_REGISTRY}";
     done
 fi
--- Makefile
+++ Makefile
@@ -15,7 +15,8 @@ else
     BASE_IMAGE_TAG := $(ALPINE_VER)-$(BASE_IMAGE_STABILITY_TAG)
 endif
 
-REPO = wodby/nginx
+REGISTRY ?= docker.io
+REPO = $(REGISTRY)/wodby/nginx
 NAME = nginx-$(NGINX_MINOR_VER)
 
 ifneq ($(STABILITY_TAG),)
